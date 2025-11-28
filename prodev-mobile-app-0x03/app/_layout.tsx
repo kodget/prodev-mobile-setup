c193013daf41607c82c7ec6c419b5bac5b425730
Muhammad <muminmuhsina1@gmail.com>
2025-11-28 14:37:17 -0800

    feat: setup screens, constants, styles and navigation layout

--- prodev-mobile-app-0x03/app/_layout.tsx
+++ prodev-mobile-app-0x03/app/_layout.tsx
@@ -1,4 +1,5 @@
 import { Stack } from "expo-router";
+import { View } from "react-native";
 
 export default function RootLayout() {
   return (
@@ -6,6 +7,10 @@ export default function RootLayout() {
       screenOptions={{
         headerShown: false,
       }}
-    />
+    >
+      <Stack.Screen name="index" />
+      <Stack.Screen name="join" />
+      <Stack.Screen name="signin" />
+    </Stack>
   );
 }
