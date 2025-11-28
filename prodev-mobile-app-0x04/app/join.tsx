200a2b4b1358066406a914f2bf5df9aa1612f6eb
Muhammad <muminmuhsina1@gmail.com>
2025-11-28 14:46:14 -0800

    more enemies

---
+++ prodev-mobile-app-0x04/app/join.tsx
@@ -0,0 +1,78 @@
+import { Text, TextInput, View, TouchableOpacity, Image } from "react-native";
+import { styles } from "@/styles/_join";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+
+export default function Join() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.iconsection}>
+          <Ionicons name="arrow-back" size={25} />
+          <Image source={require("@/assets/images/logo.png")} />
+        </View>
+
+        <View style={styles.titleTextGroup}>
+          <Text style={styles.titleText}>Create your</Text>
+          <Text style={styles.titleText}>Account</Text>
+          <Text style={styles.subText}>
+            Enter your details to create an account.
+          </Text>
+        </View>
+
+        <View style={styles.formGroup}>
+          <View>
+            <Text style={styles.formLabel}>Email</Text>
+            <TextInput
+              keyboardType="email-address"
+              style={styles.formControl}
+            />
+          </View>
+          <View>
+            <Text style={styles.formLabel}>Password</Text>
+            <View style={styles.formPasswordControl}>
+              <TextInput style={styles.passwordControl} secureTextEntry />
+              <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
+            </View>
+          </View>
+          <View>
+            <Text style={styles.formLabel}>Confirm Password</Text>
+            <View style={styles.formPasswordControl}>
+              <TextInput style={styles.passwordControl} secureTextEntry />
+              <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
+            </View>
+          </View>
+        </View>
+
+        <TouchableOpacity style={styles.primaryButton}>
+          <Text style={styles.buttonText}>Join now</Text>
+        </TouchableOpacity>
+
+        <View style={styles.dividerGroup}>
+          <View style={styles.divider}></View>
+          <Text style={styles.dividerText}>OR</Text>
+          <View style={styles.divider}></View>
+        </View>
+
+        <View style={styles.secondaryButtonGroup}>
+          <TouchableOpacity style={styles.secondaryButton}>
+            <Image source={require("@/assets/images/google.png")} />
+            <Text style={styles.secondaryButtonText}>Continue with Google</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.secondaryButton}>
+            <Image source={require("@/assets/images/facebook.png")} />
+            <Text style={styles.secondaryButtonText}>
+              Continue with Facebook
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.signupgroup}>
+          <Text style={styles.signupTitleText}>Already have an account? </Text>
+          <Text style={styles.signupSubTitleText}>Sign in</Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
